1f548e1bb75d043383a05b0633f3a83de8cad838
Ben Degeneve <poompoomcha@gmail.com>
2022-01-07 16:19:52 +0100

    Initialisation sequelize et server.js

--- back/app.js
+++ back/app.js
@@ -1,23 +1,70 @@
-const { Sequelize } = require('sequelize');
+// Import packages :
+const express = require('express');
 require('dotenv').config({ path: '../.env' });
+const path = require('path');
+const fs = require('fs');
+const helmet = require('helmet');
+// const db = require(__dirname + '/models/index');
 
-const env = process.env;
+// Import routes :
 
-// Connexion à la bdd :
-const sequelize = new Sequelize(
-  env.MYSQL_DB_NAME,
-  env.MYSQL_USERNAME,
-  env.MYSQL_PWD,
-  {
-    host: 'localhost',
-    dialect: 'mysql',
-    port: env.MYSQL_PORT,
+// const env = process.env;
+
+// // Infos de connexion :
+// const sequelize = new Sequelize(
+//   env.MYSQL_DB_NAME,
+//   env.MYSQL_USERNAME,
+//   env.MYSQL_PWD,
+//   {
+//     host: 'localhost',
+//     dialect: 'mysql',
+//     port: env.MYSQL_PORT,
+//   }
+// );
+
+// Connexion :
+// db
+//   .authenticate()
+//   .then(() => console.log('Connexion à MySql établie.'))
+//   .catch((err) => {
+//     console.log('connexion à MySql échouée.', err);
+//   });
+
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(helmet());
+
+// Configuration des headers :
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization'
+  );
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PUT, DELETE, PATCH, OPTIONS'
+  );
+  next();
+});
+
+// Création du dossier ./images s'il n'existe pas :
+fs.access('./images', fs.constants.F_OK, (error) => {
+  if (error) {
+    fs.mkdir('./images', (error) => {
+      if (error) {
+        throw error;
+      } else {
+        console.log('Création du dossier ./images.');
+      }
+    });
   }
-);
+});
 
-try {
-  sequelize.authenticate();
-  console.log('Connexion à MySql établie !');
-} catch (error) {
-  console.error('Connexion à MySql échouée : ', error);
-}
+// Chemin du dossier vers les images :
+app.use('/images', express.static(path.join(__dirname, 'images')));
+
+// Routage
+
+module.exports = app;
