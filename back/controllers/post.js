e387130f0907c04f5a290d34563ab09a9054091e
Ben Degeneve <poompoomcha@gmail.com>
2022-01-24 18:52:53 +0100

    Finders basiques

--- back/controllers/post.js
+++ back/controllers/post.js
@@ -1,3 +1,45 @@
-exports.publicationTest = (req, res, next) => {
-  res.status(200).json({ message: 'Hello Home' });
+const db = require('../models');
+
+// Get all posts:
+exports.getAllPosts = (req, res, next) => {
+  // Limitation de date ? (where: {updatedAt < 1j} ?)
+  // Ou findAndCountAll avec limite de posts visibles par pages ?
+  db.Post.findAll()
+    .then((posts) => {
+      res.status(200).json({ posts });
+    })
+    .catch((error) => {
+      res.status(404).json({ error });
+    });
 };
+
+// Get one post by postId:
+exports.getOnePostById = (req, res, next) => {
+  db.Post.findByPk(req.params.id)
+    .then((post) => {
+      res.status(200).json({ post });
+    })
+    .catch((error) => {
+      res.status(404).json({ error });
+    });
+};
+
+// Get all posts by type:
+exports.getAllPostByType = (req, res, next) => {
+  db.Post.findAll({ where: { type: req.params.type } })
+    .then((posts) => {
+      res.status(200).json({ posts });
+    })
+    .catch((error) => {
+      res.status(404).json({ error });
+    });
+};
+
+// Get all posts by date:
+
+// Get all posts by profileId:
+// Get posts by title, or part of title:
+// Get all important posts:
+// Create a post:
+// Modify a post:
+// Delete a post:
--- back/routes/post.js
+++ back/routes/post.js
@@ -5,6 +5,6 @@ const auth = require('../middleware/auth');
 
 const postCtrl = require('../controllers/post');
 
-router.get('/home', auth, postCtrl.publicationTest);
+
 
 module.exports = router;
